ab4471c99473772c084f1b1f96a40444bd439151
kdtTetz <xenosign@naver.com>
2024-07-21 01:55:05 +0900

    Update UserMain.java

--- src/main/java/org/example/UserMain.java
+++ src/main/java/org/example/UserMain.java
@@ -2,20 +2,11 @@ package org.example;
 
 public class UserMain {
     public static void main(String[] args) {
-        UserDao userDao = new UserDao();
-        // 회원 추가
-        userDao.create("tetz2", "1234");
-        // 추가 회원 조회
-        userDao.getAllUsers();
+        UserDao userDao = new UserDao();        
 
-        // 회원 수정 메서드 실행
-        userDao.updateUser(4, "lhs", "abcd");
-        // 수정 회원 조회
+        // 이름이 출력 안되는 회원 조회 메서드
         userDao.getAllUsers();
-
-        // id가 4인 회원 삭제 메서드 실행
-        userDao.deleteUser(4);
-        // 삭제 회원 조회
-        userDao.getAllUsers();
+        // 이름이 출력되는 회원 조회 메서드
+        userDao.getAllUsersWithName();
     }
 }
